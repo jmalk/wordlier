095028c37bbf3719340c46436adb0870209ab2d7
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2022-01-14 11:50:57 +0000

    Display letters in the UI

--- index.js
+++ index.js
@@ -4,13 +4,23 @@ const alphabet = 'abcdefghijklmnopqrstuvwxyz'
 let state = [];
 
 const onKeydown = ({ key }) => {
-  console.log(key);
   if (state.length < wordLength && alphabet.includes(key)) {
     state.push(key);
   }
   if (key === 'Backspace') {
     state.pop();
   }
-  console.log(state);
+  updateUI(state);
 };
+
 document.addEventListener('keydown', onKeydown);
+
+const $row = document.querySelector('.row');
+const $squares = $row.querySelectorAll('.square');
+
+const updateUI = (state) => {
+  $squares.forEach((square, i) => {
+    // Set all empty squares to empty string to handle Backspace.
+    square.textContent = state[i] || '';
+  })
+}
