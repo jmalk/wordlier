eb528d553c96f0ab39276b7272ed6a57f2b80945
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2022-01-14 11:40:11 +0000

    Add simple state to store guess

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log(`JavaScript loaded`);
+const wordLength = 6;
+const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+let state = [];
+
+const onKeydown = ({ key }) => {
+  console.log(key);
+  if (state.length < wordLength && alphabet.includes(key)) {
+    state.push(key);
+  }
+  if (key === 'Backspace') {
+    state.pop();
+  }
+  console.log(state);
+};
+document.addEventListener('keydown', onKeydown);
