a49402dbf77539fe637fd3e9cc8b243abdf452ce
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2022-01-15 21:26:04 +0000

    Check the player's guess

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
 const wordLength = 6;
 const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
-let state = [];
+const target = ['l', 'e', 't', 't', 'e', 'r'];
+const guess = [];
+let locked = false;
 
 const onKeydown = ({ key }) => {
-  if (state.length < wordLength && alphabet.includes(key)) {
-    state.push(key);
+  if (guess.length < wordLength && alphabet.includes(key)) {
+    guess.push(key);
   }
   if (key === 'Backspace') {
-    state.pop();
+    guess.pop();
   }
-  updateUI(state);
+  if (key === 'Enter' && guess.length === 6) {
+    locked = true;
+  }
+  updateUI(guess, locked, target);
 };
 
 document.addEventListener('keydown', onKeydown);
 
+const getResult = (letter, position, target) => {
+  if (target[position] === letter) {
+    return 'correct';
+  }
+  if (target.includes(letter)) {
+    return 'present';
+  }
+  return 'incorrect';
+};
+
 const $row = document.querySelector('.row');
 const $squares = $row.querySelectorAll('.square');
 
-const updateUI = (state) => {
+const updateUI = (guess, locked, target) => {
   $squares.forEach((square, i) => {
     // Set all empty squares to empty string to handle Backspace.
-    square.textContent = state[i] || '';
+    square.textContent = guess[i] || '';
   });
+
+  if (locked) {
+    $squares.forEach((square, i) => {
+      square.classList.add(getResult(guess[i], i, target));
+    });
+  }
 };
